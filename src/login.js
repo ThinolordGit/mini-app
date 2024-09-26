215540b05b94d376f53c39c5a2b3a084182122a4
ThinolordGit <othnieldos@gmail.com>
2024-09-26 18:15:51 +0100

    first commit

---
+++ src/login.js
@@ -0,0 +1,42 @@
+// src/Login.js
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Logique pour envoyer le nom d'utilisateur et le mot de passe au serveur
+    console.log('Login Submitted', { username, password });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            required 
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            required 
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
